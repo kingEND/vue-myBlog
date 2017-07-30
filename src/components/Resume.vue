0da626dbc21ce4d7366ec5f68f563d3f006436c8
kingEND <2578038306@qq.com>
2017-07-30 12:33:00 +0800

    修改简历的样式和处理逻辑放在了vuex里面了

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,14 +1,13 @@
 <template>
   <div padding="10">
     <div>
-      <i class="iconfont icon-sousuo-gjl"></i>
       <avator flex="center"></avator>
       <div flex="center">
-        <p><span class="tips">格言:</span><span>&nbsp;&nbsp;技术之路漫漫，不亦乎坚持到底</span></p>
+        <p marginUp="10"><span class="tips">格言:</span><span>&nbsp;&nbsp;技术之路漫漫，不亦乎坚持到底</span></p>
       </div>
       <p flex="center" class="title">博主简历</p>
       <div>
-        <p class="tips">技能点：</p>
+        <p marginUp="10" class="tips">技能点：</p>
         <p class="point">掌握运用vue+vue-router+vuex+axios+koa技术进行app和网页前端开发</p>
         <p class="point">熟练的运用vue开发的样式框架比如ElementUI，museUI</p>
         <p class="point">熟练运用普通的样式框架比如bootstrap,semanticUI</p>
@@ -16,30 +15,22 @@
         <p class="point">了解mysql数据库的基本操作</p>
       </div>
       <div>
-        <p class="tips">自我简介：</p>
+        <p marginUp="10" class="tips">自我简介：</p>
         <p class="smary">本人软件工程专业，本科实习生，喜欢和别人交流技术，在校期间参加了莞香校创队，
           并负责团队的官网开发，网站地址如下：http://www.gxgk.cc,之后在牛牛公司担任前端开发人员。主要负责公司的i家i户app的开发和维护
           使用vue编写前端交互组件，和扩充UI库的组件</p>
       </div>
     </div>
-    <button @click="getTest">按钮</button>
   </div>
 </template>
 
 <script>
   import Avator from '@/common/Avator.vue'
-  import {mapGetters, mapActions} from 'vuex'
   export default {
     data () {
       return {}
     },
-    methods: {
-      getTest () {
-        this.getList({url: `/v2/movie/top250`})
-      },
-      ...mapGetters(['getMoveList']),
-      ...mapActions(['getList'])
-    },
+    methods: {},
     components: {Avator}
   }
 </script>
@@ -49,12 +40,13 @@
     display: flex;
     justify-content: center;
   }
-
   .point {
     position: relative;
     margin-left: 50px;
   }
-
+  .point + .point {
+    margin-top: 5px;
+  }
   .point:before {
     display: block;
     content: '';
@@ -76,6 +68,7 @@
     z-index: 1;
     position: relative;
     padding-bottom: 5px;
+    padding-top: 10px;
   }
 
   .title:before {
@@ -105,7 +98,7 @@
 
   .smary {
     font-size: 12px;
-    line-height: 20px;
+    line-height: 24px;
     margin-bottom: 50px;
   }
 </style>
